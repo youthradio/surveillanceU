f25068e3a25a4d7d145a22db82875738d53178db
radames <radames@users.noreply.github.com>
2021-06-24 17:40:46 +0000

    deploy: 4633a559032271dc83c4e44d804a5a547ff06a82

---
+++ _nuxt/static/1624556445/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhas-virtual-proctoring-gone-too-far\u002F_nuxt\u002Fstatic\u002F1624556445",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fhas-virtual-proctoring-gone-too-far\u002F",assetsPath:"\u002Fhas-virtual-proctoring-gone-too-far\u002F_nuxt\u002F",cdnURL:a}}}}(null));
